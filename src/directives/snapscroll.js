109e638336da6b5520ef77bde8533d940e1730de
Joel Mukuthu <joelmukuthu@gmail.com>
2014-10-15 23:20:02 +0300

    initial snapscroll definition and tests

--- src/directives/snapscroll.js
+++ src/directives/snapscroll.js
@@ -1,3 +1,45 @@
 'use strict';
 
-angular.module('snapscroll');
+var scopeObject = {
+  snapIndex: '=?',
+  snapHeight: '=?',
+  beforeSnap: '&',
+  afterSnap: '&'
+};
+
+var registerWatchers = function (scope) {
+  scope.$watch('snapIndex', function (snapIndex) {
+    if (angular.isUndefined(snapIndex)) {
+      scope.snapIndex = 0;
+      return;
+    }
+  });
+};
+
+var snapscrollAsAnAttribute = [
+  function () {
+    return {
+      restrict: 'E',
+      scope: scopeObject,
+      link: function (scope) {
+        registerWatchers(scope);
+      }
+    };
+  }
+];
+
+var snapscrollAsAnElement = [
+  function () {
+    return {
+      restrict: 'A',
+      scope: scopeObject,
+      link: function (scope) {
+        registerWatchers(scope);
+      }
+    };
+  }
+];
+
+angular.module('snapscroll')
+  .directive('snapscroll', snapscrollAsAnAttribute)
+  .directive('snapscroll', snapscrollAsAnElement);
--- test/spec/directives/snapscroll.js
+++ test/spec/directives/snapscroll.js
@@ -2,36 +2,60 @@
 
 describe('Directive: snapscroll', function () {
 
-//  beforeEach(module('snapscroll'));
-//
-//  var $compile,
-//      $scope;
-//  
-//  function compileELement(html, appendToBody) {
-//    var body,
-//        element = angular.element(html);
-//    
-//    if (!angular.isDefined(appendToBody)) {
-//      appendToBody = false;
-//    }
-//    
-//    if (appendToBody) {
-//      body = angular.element(document).find('body');
-//      body.append(element);
-//    }
-//    
-//    element = $compile(element)($scope);
-//    $scope.$digest();
-//    
-//    return element;
-//  }
-//
-//  beforeEach(inject(function (_$compile_, _$rootScope_) {
-//    $compile = _$compile_;
-//    $scope = _$rootScope_.$new();
-//  }));
-//  
-//  afterEach(function () {
-//    angular.element(document).find('body').empty();
-//  });
+  beforeEach(module('snapscroll'));
+
+  var $compile,
+      $scope;
+  
+  function compileELement(html, appendToBody) {
+    var body,
+        element = angular.element(html);
+    
+    if (!angular.isDefined(appendToBody)) {
+      appendToBody = false;
+    }
+    
+    if (appendToBody) {
+      body = angular.element(document).find('body');
+      body.append(element);
+    }
+    
+    element = $compile(element)($scope);
+    $scope.$digest();
+    
+    return element;
+  }
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $scope = _$rootScope_.$new();
+  }));
+  
+  afterEach(function () {
+    angular.element(document).find('body').empty();
+  });
+  
+  it('can be declared as an attribute', function () {
+    compileELement('<div snapscroll="" snap-index="snapIndex"></div>');
+    expect($scope.snapIndex).toBeDefined();
+  });
+  
+  it('can be declared as an element', function () {
+    compileELement('<snapscroll snap-index="snapIndex"></snapscroll>');
+    expect($scope.snapIndex).toBeDefined();
+  });
+  
+  describe('as an attribute, it', function () {
+    it('defaults snapIndex to zero', function () {
+      compileELement('<div snapscroll="" snap-index="snapIndex"></div>');
+      expect($scope.snapIndex).toBe(0);
+    });
+  });
+  
+  describe('as an element, it', function () {
+    it('defaults snapIndex to zero', function () {
+      compileELement('<snapscroll snap-index="snapIndex"></snapscroll>');
+      expect($scope.snapIndex).toBe(0);
+    });
+  });
 });
