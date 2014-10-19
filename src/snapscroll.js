4f8db58e2d0d5a596742d5fb570a31e8429ccbfa
Joel Mukuthu <joelmukuthu@gmail.com>
2014-10-19 16:37:39 +0300

    added duScroll module dependency
    cleaned up module tests

--- src/snapscroll.js
+++ src/snapscroll.js
@@ -1,4 +1,17 @@
 'use strict';
 
+function easeInOutQuad(t, b, c, d) {
+  t /= d/2;
+  if (t < 1) {
+    return c/2*t*t + b;
+  }
+  t--;
+  return -c/2 * (t*(t-2) - 1) + b;
+}
+
 angular
-  .module('snapscroll', []);
+  .module('snapscroll', [
+    'duScroll'
+  ])
+  .value('duScrollDuration', 800)
+  .value('duScrollEasing', easeInOutQuad);
--- test/spec/snapscroll.js
+++ test/spec/snapscroll.js
@@ -13,23 +13,14 @@ describe('Module: snapscroll', function () {
   });
   
   describe('as an attribute', function () {
-    it('requires duScroll.scrollHelpers', function () {
+    it('requires duScroll', function () {
       var app;
 
       expect(function () {
-        // app = angular.module('snapscroll', ['duScroll.scrollHelpers']);
-        app = angular.module('duScroll.scrollHelpers');
+        app = angular.module('duScroll');
       }).not.toThrow();
 
       expect(app).toBeDefined();
-    });
-    
-    it('sets the duScrollEasing to ease-in-out (snapscrollEaseInOut)', function () {
-      expect(angular.value('duScrollEasing')).toBe(angular.value('snapscrollEaseInOut'));
-    });
-
-    it('sets the duScrollDuration to 400', function () {
-      expect(angular.value('duScrollDuration')).toBe(angular.value(400));
     });
   });
 });
