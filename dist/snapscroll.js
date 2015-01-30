a93cd7e02554859083ff4deacfa4d45c340c1d3e
Joel Mukuthu <joelmukuthu@gmail.com>
2015-01-30 04:45:26 +0300

    added missing closure

--- dist/snapscroll.js
+++ dist/snapscroll.js
@@ -1,22 +1,23 @@
-'use strict';
+(function () {
+  'use strict';
 
-function easeInOutQuad(t, b, c, d) {
-  t /= d/2;
-  if (t < 1) {
-    return c/2*t*t + b;
+  function easeInOutQuad(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) {
+      return c/2*t*t + b;
+    }
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
   }
-  t--;
-  return -c/2 * (t*(t-2) - 1) + b;
-}
 
-angular
-  .module('snapscroll', [])
-  .value('defaultSnapscrollScrollEasing', easeInOutQuad)
-  .value('defaultSnapscrollScrollDelay', 250)
-  .value('defaultSnapscrollSnapDuration', 800)
-  .value('defaultSnapscrollSnapToWindowHeightResizeDelay', 400)
-  .value('defaultSnapscrollBindScrollTimeout', 400);
-
+  angular
+    .module('snapscroll', [])
+    .value('defaultSnapscrollScrollEasing', easeInOutQuad)
+    .value('defaultSnapscrollScrollDelay', 250)
+    .value('defaultSnapscrollSnapDuration', 800)
+    .value('defaultSnapscrollSnapToWindowHeightResizeDelay', 400)
+    .value('defaultSnapscrollBindScrollTimeout', 400);
+})();
 (function () {
   'use strict';
 
--- src/snapscroll.js
+++ src/snapscroll.js
@@ -1,18 +1,20 @@
-'use strict';
+(function () {
+  'use strict';
 
-function easeInOutQuad(t, b, c, d) {
-  t /= d/2;
-  if (t < 1) {
-    return c/2*t*t + b;
+  function easeInOutQuad(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) {
+      return c/2*t*t + b;
+    }
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
   }
-  t--;
-  return -c/2 * (t*(t-2) - 1) + b;
-}
 
-angular
-  .module('snapscroll', [])
-  .value('defaultSnapscrollScrollEasing', easeInOutQuad)
-  .value('defaultSnapscrollScrollDelay', 250)
-  .value('defaultSnapscrollSnapDuration', 800)
-  .value('defaultSnapscrollSnapToWindowHeightResizeDelay', 400)
-  .value('defaultSnapscrollBindScrollTimeout', 400);
+  angular
+    .module('snapscroll', [])
+    .value('defaultSnapscrollScrollEasing', easeInOutQuad)
+    .value('defaultSnapscrollScrollDelay', 250)
+    .value('defaultSnapscrollSnapDuration', 800)
+    .value('defaultSnapscrollSnapToWindowHeightResizeDelay', 400)
+    .value('defaultSnapscrollBindScrollTimeout', 400);
+})();
