53aada0df1de379bc829ec27ccb69b514994d372
Joel Mukuthu <joelmukuthu@gmail.com>
2015-04-04 00:03:03 +0200

    Removed trailing whitespace

--- test/spec/snapscroll.js
+++ test/spec/snapscroll.js
@@ -1,41 +1,41 @@
 'use strict';
 
 describe('Module: snapscroll', function () {
-  
+
   it('is created', function () {
     var app;
-    
+
     expect(function () {
       app = angular.module('snapscroll');
     }).not.toThrow();
-    
+
     expect(app).toBeDefined();
   });
-  
-  describe('', function () {
-    
+
+  describe('registers the', function () {
+
     beforeEach(module('snapscroll'));
-    
-    it('registers the defaultSnapscrollScrollEasing', inject(function (defaultSnapscrollScrollEasing) {
+
+    it('defaultSnapscrollScrollEasing', inject(function (defaultSnapscrollScrollEasing) {
       expect(angular.isFunction(defaultSnapscrollScrollEasing)).toBe(true);
     }));
-    
-    it('registers the defaultSnapscrollScrollDelay', inject(function (defaultSnapscrollScrollDelay) {
+
+    it('defaultSnapscrollScrollDelay', inject(function (defaultSnapscrollScrollDelay) {
       expect(angular.isNumber(defaultSnapscrollScrollDelay)).toBe(true);
     }));
-    
-    it('registers the defaultSnapscrollSnapDuration', inject(function (defaultSnapscrollSnapDuration) {
+
+    it('defaultSnapscrollSnapDuration', inject(function (defaultSnapscrollSnapDuration) {
       expect(angular.isNumber(defaultSnapscrollSnapDuration)).toBe(true);
     }));
-    
-    it('registers the defaultSnapscrollResizeDelay', inject(function (defaultSnapscrollResizeDelay) {
+
+    it('defaultSnapscrollResizeDelay', inject(function (defaultSnapscrollResizeDelay) {
       expect(angular.isNumber(defaultSnapscrollResizeDelay)).toBe(true);
     }));
-    
-    it('registers the defaultSnapscrollBindScrollTimeout', inject(function (defaultSnapscrollBindScrollTimeout) {
+
+    it('defaultSnapscrollBindScrollTimeout', inject(function (defaultSnapscrollBindScrollTimeout) {
       expect(angular.isNumber(defaultSnapscrollBindScrollTimeout)).toBe(true);
     }));
-    
+
   });
-  
+
 });
