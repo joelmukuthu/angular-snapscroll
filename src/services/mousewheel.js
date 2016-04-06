6b211d5dd421c6f6a94ef962166d5def2d2153b3
Joel Mukuthu <joelmukuthu@gmail.com>
2016-04-06 23:45:14 +0200

    Minor refactor: removed unnecessary function definition

--- src/services/mousewheel.js
+++ src/services/mousewheel.js
@@ -4,47 +4,42 @@
   var snapscroll = angular.module('snapscroll');
 
   snapscroll.factory('mousewheel', [function () {
-    function onWheel(e, up, down) {
-      var delta;
-
-      if (e.originalEvent) {
-        e = e.originalEvent;
-      }
-
-      delta = Math.max(-1, Math.min(1, (e.wheelDelta || -(e.deltaY || e.detail))));
-      if (isNaN(delta) || delta === 0) {
-        return;
-      }
-
-      if (delta > 0) {
-        if (up) {
-          up(e);
-        }
-      } else {
-        if (down) {
-          down(e);
-        }
-      }
-    }
-
     return {
       bind: function (element, callbacks) {
         callbacks = callbacks || {};
+
         if (angular.isDefined(callbacks.up) && !angular.isFunction(callbacks.up)) {
           throw new Error('The \'up\' callback must be a function');
         }
-
         if (angular.isDefined(callbacks.down) && !angular.isFunction(callbacks.down)) {
           throw new Error('The \'down\' callback must be a function');
         }
-
         if (!angular.isDefined(callbacks.up) && !angular.isDefined(callbacks.down)) {
           throw new Error('At least one callback (\'up\' or \'down\') must be provided');
         }
 
         function bindWheel(e) {
-          onWheel(e, callbacks.up, callbacks.down);
+          if (e.originalEvent) {
+            e = e.originalEvent;
+          }
+
+          var delta;
+          delta = Math.max(-1, Math.min(1, (e.wheelDelta || -(e.deltaY || e.detail))));
+          if (isNaN(delta) || delta === 0) {
+            return;
+          }
+
+          if (delta > 0) {
+            if (callbacks.up) {
+              callbacks.up(e);
+            }
+          } else {
+            if (callbacks.down) {
+              callbacks.down(e);
+            }
+          }
         }
+
         element.data('snapscroll-bindWheel', bindWheel);
         element.on('wheel mousewheel onmousewheel', bindWheel);
       },
