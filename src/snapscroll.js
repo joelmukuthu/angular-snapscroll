483acc2c30d0d91a3d2bc0f9db116e0625ab96a8
Joel Mukuthu <joelmukuthu@gmail.com>
2017-03-06 10:48:15 +0100

    Remove unnecessary function scopes

--- src/snapscroll.js
+++ src/snapscroll.js
@@ -1,10 +1,8 @@
-(function () {
-    angular
-        .module('snapscroll', ['wheelie', 'scrollie'])
-        .value('defaultSnapscrollScrollEasing', undefined)
-        .value('defaultSnapscrollScrollDelay', 250)
-        .value('defaultSnapscrollSnapDuration', 800)
-        .value('defaultSnapscrollResizeDelay', 400)
-        .value('defaultSnapscrollBindScrollTimeout', 400)
-        .value('defaultSnapscrollPreventDoubleSnapDelay', 1000);
-})();
+angular
+    .module('snapscroll', ['wheelie', 'scrollie'])
+    .value('defaultSnapscrollScrollEasing', undefined)
+    .value('defaultSnapscrollScrollDelay', 250)
+    .value('defaultSnapscrollSnapDuration', 800)
+    .value('defaultSnapscrollResizeDelay', 400)
+    .value('defaultSnapscrollBindScrollTimeout', 400)
+    .value('defaultSnapscrollPreventDoubleSnapDelay', 1000);
